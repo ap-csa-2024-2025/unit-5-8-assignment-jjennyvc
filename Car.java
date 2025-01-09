84463b9156c492d03fba358ad113b09758203e10
jjennyvc <153659964+jjennyvc@users.noreply.github.com>
2025-01-09 19:19:28 +0000

    skibidi toilet

--- Car.java
+++ Car.java
@@ -1,4 +1,36 @@
 public class Car
 {
-  
+  private String make;
+  private String model;
+  private int year;
+  private double mpg;
+
+  public Car()
+  {
+    make = "none";
+    model = "none";
+    year = 2000;
+    mpg = 0;
+  }
+   public Car(String make, String model, int year, double mpg)
+   {
+    this.make = make;
+    this.year = year;
+
+    if (this.year > 2022)
+    {
+        this.year = 2022;
+    }
+    if(this.year < 1885)
+    {
+        this.year = 2000;
+    }
+    
+    this.mpg = mpg;
+    if(this.mpg < 0)
+    {
+        this.mpg = 0;
+    }
+   }
+   public static CalculateID
 }
